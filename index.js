83bbae598d1c472e902b2cff79a33e747af24516
shan <shan@ssvet.info>
2021-06-01 16:44:31 +0700

    Fix: minor refactoring

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from 'express';
 
 import userRouter from './UserRouter.js';
 
-const PORT = 5000;
+const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.json());
