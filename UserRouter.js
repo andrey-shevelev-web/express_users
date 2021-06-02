92d7b56c9d5125cbfe37090ae85a0ed3e36bfbbf
shan <shan@ssvet.info>
2021-06-02 11:24:01 +0700

    Adds: post-route users

--- UserRouter.js
+++ UserRouter.js
@@ -3,7 +3,8 @@ import UserController from './UserController.js';
 
 const userRouter = new Router();
 
-userRouter.get('/users', UserController.getPage);
 userRouter.get('/users-all', UserController.getAll);
+userRouter.get('/users', UserController.getPage);
+userRouter.post('/users', UserController.getPage);
 
 export default userRouter;
